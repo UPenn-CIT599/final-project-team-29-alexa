161bc0bc9380084f0c84fbb2788ad6e0378bc61d
JQLu <lujiaqi19940905@gmail.com>
2019-12-01 01:19:04 -0500

    Update SnakeGame.java

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -11,10 +11,12 @@ public class SnakeGame {
 
 		ArrayList<BodyPart> body = new ArrayList<BodyPart>();
 		BodyPart initial = new BodyPart(25, 25); 
+		BodyPart secondary = new BodyPart(24, 25);
 		body.add(initial);
+		body.add(secondary);
 		
 		Apple apple = new Apple(random.nextInt(51), random.nextInt(51));
-		char input;
+		char direction = 'd';
 		
 		while (!isGameOver(body)) {
 			PennDraw.clear();
@@ -30,18 +32,30 @@ public class SnakeGame {
 			//takes user instruction: direction(WASD), keep a copy of current head location,
 			//update next head location
 			BodyPart currentHead = body.get(0);
-			char direction = 'd';
 			
 			if (PennDraw.hasNextKeyTyped()) {
-				input = Character.toLowerCase(PennDraw.nextKeyTyped());
-				direction = input; 
+				direction = Character.toLowerCase(PennDraw.nextKeyTyped());
 			}
-			
-			
+			if (direction == 'd')
+				body.get(0).setxCoor(body.get(0).getxCoor()+1);
+			else if (direction == 'a')
+				body.get(0).setxCoor(body.get(0).getxCoor()-1);
+			else if (direction == 'w')
+				body.get(0).setyCoor(body.get(0).getyCoor()+1);
+			else if (direction == 's')
+				body.get(0).setyCoor(body.get(0).getyCoor()-1);
 			
 			
 			//if eats(head coor = apple), apple becomes new head, all bodypart pushed back 1 index, update apple 
 			//if no eats, all body parts replaced by index-1
+			for (int j = (body.size() - 1); j > 1; j--) 
+				body.set(j, body.get(j-1));
+			body.set(1, currentHead);
+			
+			if (currentHead.getxCoor() == apple.getxCoor() && currentHead.getyCoor() == apple.getyCoor()) {} 
+			else 
+				body.remove(body.size()-1);	
+			
 			
 			
 			PennDraw.advance();
