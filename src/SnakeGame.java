7f8b5856b7457234f21dd4ee155e656281cdd273
Sam <ssydyc@gmail.com>
2019-12-01 18:46:05 -0800

    fix issues of index to make game work

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -14,13 +14,15 @@ public class SnakeGame {
 		BodyPart secondary = new BodyPart(24, 25);
 		body.add(initial);
 		body.add(secondary);
-		
+
 		Apple apple = new Apple(random.nextInt(51), random.nextInt(51));
-		char direction = 'd';
-		
-		while (!isGameOver(body)) {
-			PennDraw.clear();
-			
+		char direction = 'd'; while (!isGameOver(body)) {
+			try {
+				Thread.sleep(200);
+			} catch(InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
+
 			//Draw current body
 			PennDraw.clear();
 			for (BodyPart o: body)
@@ -31,34 +33,28 @@ public class SnakeGame {
 			
 			//takes user instruction: direction(WASD), keep a copy of current head location,
 			//update next head location
-			BodyPart currentHead = body.get(0);
-			
 			if (PennDraw.hasNextKeyTyped()) {
 				direction = Character.toLowerCase(PennDraw.nextKeyTyped());
 			}
 			if (direction == 'd')
-				body.get(0).setxCoor(body.get(0).getxCoor()+1);
+				body.add(0, new BodyPart(body.get(0).getxCoor()+1, body.get(0).getyCoor()));
 			else if (direction == 'a')
-				body.get(0).setxCoor(body.get(0).getxCoor()-1);
+				body.add(0, new BodyPart(body.get(0).getxCoor()-1, body.get(0).getyCoor()));
 			else if (direction == 'w')
-				body.get(0).setyCoor(body.get(0).getyCoor()+1);
+				body.add(0, new BodyPart(body.get(0).getxCoor(), body.get(0).getyCoor()+1));
 			else if (direction == 's')
-				body.get(0).setyCoor(body.get(0).getyCoor()-1);
-			
-			
-			//if eats(head coor = apple), apple becomes new head, all bodypart pushed back 1 index, update apple 
+				body.add(0, new BodyPart(body.get(0).getxCoor(), body.get(0).getyCoor()-1));
+
+			//if eats(head coor = apple), apple becomes new head, all bodypart pushed back 1 index, update apple
 			//if no eats, all body parts replaced by index-1
-			for (int j = (body.size() - 1); j > 1; j--) 
-				body.set(j, body.get(j-1));
-			body.set(1, currentHead);
-			
-			if (currentHead.getxCoor() == apple.getxCoor() && currentHead.getyCoor() == apple.getyCoor()) {} 
-			else 
-				body.remove(body.size()-1);	
-			
-			
+			if (body.get(0).getxCoor() != apple.getxCoor() || body.get(0).getyCoor() != apple.getyCoor()) {
+				body.remove(body.size() - 1);
+			} else {
+				apple = new Apple(random.nextInt(51), random.nextInt(51));
+			}
 			
 			PennDraw.advance();
+
 		}
 		
 		//Display GAME OVER
@@ -77,7 +73,7 @@ public class SnakeGame {
 		
 		//collision with walls
 		BodyPart head = body.get(0);
-		if (head.getxCoor() > 50 || head.getyCoor()>50 ||
+		if (head.getxCoor() > 50 || head.getyCoor() > 50 ||
 			head.getxCoor() < 0 || head.getyCoor() < 0) {
 			return true;
 		}
